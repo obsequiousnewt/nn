a4e3b9169258800596b804bccdccc20e69e0ef25
zeb <obsequiousnewt@gmail.com>
2016-11-15 13:47:17 -0600

    Fix bug but also do some significant rewrites 1. The bug was just a missing import line. 2. The snake data was being duplicated (and only changed in one place), so I changed the gameboard to just be a list of food positions (and removed the redundant 'body' mark from the snake.) Since we only interact with each kind of list once this makes things easier to work with anyway. 3. That said we do need a function to transform those two lists into a single 2x2 grid for the NN to read. That's going to be a pain to write. 4. Rewrote the update function to be less redundant. 5. Removed the "add fruit" thing because it involves randomness and we want to put that at the topmost level we can. Also doesn't it usually get spawned when you *do* eat a fruit, not when you don't?

--- snake.hs
+++ snake.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE ViewPatterns #-}
+
 import qualified Data.Sequence as S
+import Data.Sequence (Seq, fromList, index, elemIndexR, (<|))
+import Data.Foldable
 
 -- Setup for game uses PM's code for Tic-Tac-Toe from HW 3
 -- https://github.com/grinnell-cs/395-homework-03
@@ -7,13 +11,13 @@ import qualified Data.Sequence as S
 data Movement = InputUp | InputDown | InputLeft | InputRight deriving (Eq, Show)
 
 -- Marks that determine what is on each space in the game
-data Mark = Body | Food deriving (Eq, Show)
+data Mark = Empty | Body | Food deriving (Eq, Show)
 
+--           x    y
 type Loc = (Int, Int)
-type GameSpace = (Mark, Loc)
 
--- Position is a snapshot of the current state of the game
-type Position = [GameSpace]
+type Position = ([Loc],Seq Loc)
+
 boardRows = 8
 boardCols = 8
 
@@ -39,78 +43,51 @@ boardCols = 8
 --  -----------------
 --   1 2 3 4 5 6 7 8
 -- with B being the Body and F being food.
-newGame :: Position
-newGame = [(Body, (4,3)), (Food, (7,7))]
 
-newSnake :: S.Seq GameSpace
-newSnake = S.fromList [(Body,(4,3))]
+newGame :: Position
+newGame = ([(7,7)],fromList [(4,3)])
+
+-- generates a gameboard in the format of a 2D array that can be easily read by the neural net
+
+toGameboard :: Position -> [[Mark]]
+toGameboard = undefined
 
 -- Functions to help search within the snake
-snakeHead :: S.Seq GameSpace -> Loc
-snakeHead snake = snd (S.index snake 0)
+snakeHead :: Seq Loc -> Loc
+snakeHead snake = index snake 0
 
+removeFood :: Loc -> [Loc] -> [Loc]
+removeFood food foods = filter (/=food) foods
 
-isValidMove :: Position -> Loc -> Bool
-isValidMove pos loc = (fst loc) < 0 && (fst loc) <= boardCols && (snd loc) > 0 && (snd loc) <= boardRows && ((filter (\n -> (snd n) == loc) pos) /= [(Body, loc)])
-
-
-isFood :: Position -> Loc -> Bool
-isFood pos loc = ((filter (\n -> (snd n) == loc) pos) == [(Food, loc)])
-
--- replaceFood is only called when the move is valid and the snake is
--- going to eat the food ingame. This replaces the food as the new head.
-replaceFood :: Position -> Loc -> Position
-replaceFood [] _ = []
-replaceFood (x:xs) loc
-    | (snd x) == loc = newSpace:xs
-    | otherwise      = x:(replaceFood xs loc)
-    where newSpace = (Body, loc)
-
--- generateFood takes the current frame and finds a random empty spot
--- and changes the mark to Food
-generateNewFood :: Position -> Position
-generateNewFood = undefined -- Will look up how to make random numbers later
+hitSelf :: Loc -> Seq Loc -> Bool
+hitSelf newHead snake = case (elemIndexR newHead snake) of
+    Just _  -> True
+    Nothing -> False
 
 -- generateFrame will take a player movement and if the movement
--- is valid, it will return Pos, otherwise returns nothing Since
+-- is valid, it will return Position, otherwise returns nothing since
 -- the move loses the game.
-generateFrame :: Movement -> Position -> S.Seq GameSpace -> Maybe Position
-generateFrame InputUp pos snake
-    | isValidMove pos newLoc == False = Nothing
-    | isFood pos newLoc               = Just (replaceFood pos newLoc)
-    | otherwise                       = Just (generateNewFood (toList (S.take ((S.length snake) - 1) snake)))
-    where newLoc = (((fst (snakeHead snake)) + 1), (snd (snakeHead snake)))
-generateFrame InputDown pos snake
-    | isValidMove pos newLoc == False = Nothing
-    | isFood pos newLoc               = Just (replaceFood pos newLoc)
-    | otherwise                       = Just (generateNewFood (toList (S.take ((S.length snake) - 1) snake)))
-    where newLoc = (((fst (snakeHead snake)) - 1), (snd (snakeHead snake)))
-generateFrame InputLeft pos snake
-    | isValidMove pos newLoc == False = Nothing
-    | isFood pos newLoc               = Just (replaceFood pos newLoc)
-    | otherwise                       = Just (generateNewFood (toList (S.take ((S.length snake) - 1) snake)))
-    where newLoc = ((fst (snakeHead snake)), ((snd (snakeHead snake)) - 1))
-generateFrame InputRight pos snake
-    | isValidMove pos newLoc == False = Nothing
-    | isFood pos newLoc               = Just (replaceFood pos newLoc)
-    | otherwise                       = Just (generateNewFood (toList (S.take ((S.length snake) - 1) snake)))
-    where newLoc = ((fst (snakeHead snake)), ((snd (snakeHead snake)) + 1))
 
--- Problems encountered so far:
--- 1. It doesn't compile for the weirdest reason.
---      "Variable not in scope: toList :: S.Seq GameSpace -> Position"
---    It should return [GameSpace] which is exactly what Position is.
---    Not really sure how to fix it. Maybe we'll talk to PM.
+generateFrame :: Movement -> Position -> Maybe Position
+generateFrame InputUp (foods,snake)
+    | y == boardRows = Nothing
+    | otherwise      = generateFrame' (x,y+1) (foods,snake)
+    where (x,y)   = snakeHead snake
+generateFrame InputDown (foods,snake)
+    | y == 1         = Nothing
+    | otherwise      = generateFrame' (x,y-1) (foods,snake)
+    where (x,y)   = snakeHead snake
+generateFrame InputLeft (foods,snake)
+    | x == 1         = Nothing
+    | otherwise      = generateFrame' (x-1,y) (foods,snake)
+    where (x,y)   = snakeHead snake
+generateFrame InputRight (foods,snake)
+    | x == boardCols = Nothing
+    | otherwise      = generateFrame' (x+1,y) (foods,snake)
+    where (x,y)   = snakeHead snake
 
--- 2. There is no way to update the snake due to how Haskell works.
---    The solution would be having a function that creates a new
---    sequence for the snake by finding making a Head marking and
---    have it follow the path the snake appears to have taken. only
---    problem is that if the snake traveled along its body, or in a
---    zig-zag pattern and looks like a block of Body spaces, the function
---    won't know how to create the right path. Maybe if it takes the new
---    frame and old snake sequence it might work, but maybe not. Haven't
---    tried yet.
-
--- If 1 is fixed, it might compile and then we'd just need 2 fixed. If
--- you guys want to add on or fix it, go ahead.
+generateFrame' :: Loc -> Position -> Maybe Position
+generateFrame' newHead (foods,snake)
+    | hitSelf newHead snake = Nothing
+    | elem newHead foods    = Just (removeFood newHead foods, newHead <| snake)
+    | otherwise             = Just (foods, newHead <| (S.take ((S.length snake) - 1) snake))
